5473c7867ce5a53e7b2568e6b541c06cd7c70b5d
Matt <Matt@MATTAB>
2016-05-25 10:48:25 -0400

    FIRST : Initial test branch and push from ECLIPSE
    Task-Url:

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Sel_JAVA
 
 Needs selenium
+
+Basic selenium test that runs scans on google to measure results
---
+++ Selenium Test/src/base/selenium/Main.java
@@ -0,0 +1,10 @@
+package base.selenium;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub");
+
+	}
+
+}
